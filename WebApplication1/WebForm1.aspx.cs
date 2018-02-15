8aaf9cd26af94530546f6314757695c90d197013
irjdp <rjdpgupta@gmail.com>
2018-02-15 16:51:45 +0530

    practice

--- WebApplication1/WebForm1.aspx.cs
+++ WebApplication1/WebForm1.aspx.cs
@@ -16,7 +16,7 @@ namespace WebApplication1
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Label1.Text = "hello";
+            Label1.Text = "hi";
         }
     }
 }
